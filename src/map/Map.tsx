e5df4956171b449beeb016945b1329fbf4ec4b04
bclayn24 <useraddress24@gmail.com>
2025-02-28 20:21:58 +0500

    change map style

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -65,10 +65,11 @@ export default function Map() {
         <MapUpdater chosenStation={chosenStation} />
 
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-          subdomains={["a", "b", "c", "d"]}
+          attribution='Map data © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of Humanitarian OpenStreetMap Team'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+          subdomains={["a", "b", "c"]}
         />
+
         {data.map((station) => (
           <Marker
             position={[
